d6ee34022297e69733e7ba368f052dea35af8bf1
mktcode <kontakt@markus-kottlaender.de>
2023-01-19 22:16:33 +0100

    style

--- src/Empty.vue
+++ src/Empty.vue
@@ -1,3 +1,3 @@
 <template>
-  <div class="grow flex flex-col bg-gray-50 hover:bg-gray-100 cursor-pointer" />
+  <div class="grow flex flex-col bg-gradient-to-t from-gray-100 to-gray-50 hover:bg-gray-100 cursor-pointer" />
 </template>
