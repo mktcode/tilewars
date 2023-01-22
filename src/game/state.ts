f1002f8d2b313802d2ac0e5f38ba019a93d5bf31
mktcode <kontakt@markus-kottlaender.de>
2023-01-22 22:35:46 +0100

    randomize player 2 positions

--- src/game/state.ts
+++ src/game/state.ts
@@ -16,18 +16,36 @@ const player1Units = ref<AbstractUnit[]>([
 ])
 
 const player2Units = ref<AbstractUnit[]>([
-  new Base(3, 10),
-  new Tank(2, 8),
-  new Tank(3, 8),
-  new Tank(4, 8),
-  new Soldier(2, 9),
-  new Soldier(3, 9),
-  new Soldier(4, 9),
-  new Soldier(1, 9),
-  new Soldier(5, 9),
-  new Sniper(2, 10),
-  new Sniper(4, 10),
+  new Base(),
+  new Tank(),
+  new Tank(),
+  new Tank(),
+  new Soldier(),
+  new Soldier(),
+  new Soldier(),
+  new Soldier(),
+  new Soldier(),
+  new Sniper(),
+  new Sniper(),
 ])
+
+function randomizePositions(units: AbstractUnit[]) {
+  units.forEach((unit) => {
+    let isPositionTaken = false
+    let randomPosition = { x: 0, y: 0 }
+    do {
+      randomPosition = {
+        x: Math.floor(Math.random() * 5) + 1,
+        y: Math.floor(Math.random() * 5) + 6,
+      }
+      isPositionTaken = !!units.find((unit) => unit.x === randomPosition.x && unit.y === randomPosition.y);
+    } while (isPositionTaken)
+    unit.x = randomPosition.x
+    unit.y = randomPosition.y
+  })
+}
+
+randomizePositions(player2Units.value)
 
 const player1HasBase = computed(() => player1Units.value.find((unit) => unit.health > 0 && unit.tags.includes('base')));
 const player2HasBase = computed(() => player2Units.value.find((unit) => unit.health > 0 && unit.tags.includes('base')));
