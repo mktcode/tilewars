f75da29ac50ea5e96a5b61088a496fb306bd79a9
mktcode <kontakt@markus-kottlaender.de>
2023-01-23 03:35:35 +0100

    health bar

--- src/Unit.vue
+++ src/Unit.vue
@@ -9,6 +9,8 @@ defineProps<{
 <template>
   <div :class="`grow flex flex-col items-center justify-center rounded-xl border-4 border-white text-white text-opacity-70 font-bold bg-gradient-to-t`">
     <div class="text-xs leading-none capitalize">{{ unit.tags[0] }}</div>
-    <div class="text-lg leading-none mt-1">{{ unit.health }}</div>
+    <div class="w-10 mt-1">
+      <div class="h-1 bg-white opacity-50 rounded-full" :style="`width: ${Math.floor(unit.health / unit.maxHealth * 100)}%`"></div>
+    </div>
   </div>
 </template>
